9553ed48f8d8b9e5660c67fc3b446514ca3fc853
rtugeek <rtugeek@gmail.com>
2015-11-27 18:12:14 +0800

    fixed bug

--- app/src/main/java/com/freegeek/android/sheet/MainActivity.java
+++ app/src/main/java/com/freegeek/android/sheet/MainActivity.java
@@ -11,6 +11,9 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.freegeek.android.sheet.activity.BaseActivity;
 import com.freegeek.android.sheet.bean.Event;
 import com.freegeek.android.sheet.bean.User;
@@ -58,6 +61,27 @@ public class MainActivity extends BaseActivity {
     private ProfileFragment mProfileFragment;
     private SheetFragment mSheetFragment;
     private LocationFragment mLocationFragment;
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
+
+    private void initLocation(){
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span=1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认false，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,6 +95,9 @@ public class MainActivity extends BaseActivity {
         initDrawerNavigation();
         refreshUser();
         replaceFragment(mSheetFragment);
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);    //注册监听函数
+        mLocationClient.start();
     }
 
     private void initFragments(){
@@ -141,12 +168,26 @@ public class MainActivity extends BaseActivity {
                             switch (position) {
                                 case 1:
                                     replaceFragment(mLocationFragment);
+                                    break;
                                 case 3:
+                                    break;
+                                case 4:
+                                    showToast(R.string.not_logined_in);
+                                    break;
+                            }
+                        } else {
+                            switch (position) {
+                                case 1:
+                                    replaceFragment(mLocationFragment);
+                                    break;
+                                case 3:
+                                    break;
                                 case 4:
                                     showToast(R.string.not_logined_in);
                                     break;
                             }
                         }
+
                         return false;
                     }
                 })
@@ -217,6 +258,9 @@ public class MainActivity extends BaseActivity {
             case Event.EVENT_GET_CAMERA_SHEET_PHOTO:
                 mPhotoName = event.getTag().toString();
                 break;
+            case Event.EVENT_GET_LOCATION:
+                MyApplication.updateUserLocation();
+                break;
         }
     }
 
@@ -229,12 +273,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_homepage) {
             replaceFragment(mSheetFragment);
             return true;
